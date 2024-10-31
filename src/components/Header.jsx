863ac7353ea68bee8b82e2af2b3f4c919be90997
iHarrvz <iHarrvz@hotmail.com>
2024-10-31 14:31:28 +0000

    Initial Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
         <div className="container mx-auto flex items-center justify-between">
           <h1 className="text-3xl font-bold">Flood Tracker</h1>
           <p className="text-sm md:text-lg text-gray-200 italic">
-            Stay updated with real-time flood alerts
+            Stay updated with real-time flood alerts!
           </p>
         </div>
       </header>
