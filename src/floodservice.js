f4e3143d66daf5eeb011abed1e7d61c4d8f3bf3c
iHarrvz <iHarrvz@hotmail.com>
2024-10-31 14:49:28 +0000

    Finishing Touches

--- src/floodservice.js
+++ src/floodservice.js
@@ -5,7 +5,6 @@ const API_URL = 'https://environment.data.gov.uk/flood-monitoring/id/floods';
 export const getFloodData = async () => {
   try {
     const response = await axios.get(API_URL);
-    console.log("API Response:", response.data);
     return response.data;
   } catch (error) {
     console.error('Error fetching flood data:', error);
